3d75541970c5841d46fa79f0ce39e69b5235550f
Gerry Mulvenna <git@movingwifi.com>
2017-06-07 21:31:38 +0100

    Remove ip address protection as we now need to run data-script from production workspace Signed-off-by: Gerry Mulvenna <git@movingwifi.com>

--- scripts/data-script.php
+++ scripts/data-script.php
@@ -2,13 +2,6 @@
 // script to pull data from Democracy Club csv and create JSONs
 require "functions.php";
 
-// avoid any scripting on public-facing website to avoid trashing any data files
-if ($_SERVER['SERVER_ADDR'] == "216.92.68.138")
-{
-    header("Location: /"); /* Redirect browser */
-    exit();
-}
-
 //buildRtree($elections, $outDir, $party_prefix, $party_colors);
 buildData($CSVs, $use_fields, $req_fields);
 buildPtree($elections, $party_prefix, $party_colors);
