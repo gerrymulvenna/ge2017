dad3d782e32b2df83ad097fd1b743eae59d7b1a1
Gerry Mulvenna <git@movingwifi.com>
2023-11-13 06:58:05 -0500

    catchup after server move

--- scripts/cron.php
+++ scripts/cron.php
@@ -3,8 +3,8 @@
 require "functions.php";
 
 //buildRtree($elections, $outDir, $party_prefix, $party_colors);
-buildData($CSVs, $use_fields, $req_fields, "/usr/home/mulsoft/public_html/ge2017.mulvenna.org/scripts");
-buildPtree($elections, $party_prefix, $party_colors, "/usr/home/mulsoft/public_html/ge2017.mulvenna.org/scripts/england-regions.json");
-buildCtree($elections, $party_prefix, "/usr/home/mulsoft/public_html/ge2017.mulvenna.org/scripts/england-regions.json");
+buildData($CSVs, $use_fields, $req_fields, "/usr/home/movgwifi/public_html/ge2017.mulvenna.org/scripts");
+buildPtree($elections, $party_prefix, $party_colors, "/usr/home/movgwifi/public_html/ge2017.mulvenna.org/scripts/england-regions.json");
+buildCtree($elections, $party_prefix, "/usr/home/movgwifi/public_html/ge2017.mulvenna.org/scripts/england-regions.json");
 
 ?>
